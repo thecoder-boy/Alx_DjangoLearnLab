688e9d21548f4f07562435cf9cc9fb34379de988
thecoder-boy <bielertwreiner@gmail.com>
2024-09-08 01:02:06 +0000

    all in one endpoint

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
 from .views import BookListApiView
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('books', BookListApiView)
 
 urlpatterns = [
-  path('books/', BookListApiView.as_view()),
+  # path('books/', BookListApiView.as_view()),
+  path('books-endpoint/', include(router.urls))
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from api.models import Book
 from .serializers import BookSerializer
 from rest.framework import generics
+from rest_framework import viewsets
 # Create your views here.
 
-class BookListApiView(generics.ListAPIView):
+class BookListApiView(viewsets.ModelViewSet):
   serializer_class = BookSerializer
   model = Book
   queryset = Book.objects.all()
