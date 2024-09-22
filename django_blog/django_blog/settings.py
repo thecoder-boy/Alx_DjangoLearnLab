edc7f73dd2f4292553708fbfb6189abef252ffb0
thecoder-boy <bielertwreiner@gmail.com>
2024-09-22 23:55:44 +0000

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blogdb",
+        "USER": "blogadmin",
+        "PASSWORD": "hanlEy[132]",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
